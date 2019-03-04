f78847dd9538deaf4026231baef3eb709460e6ad
Andrew Propst <apropst@gmail.com>
2019-03-03 21:19:57 -0500

    Updated bot and gitignore.

--- bot.js
+++ bot.js
@@ -1,1 +1,10 @@
-import hue from "node-hue-api";
+const hue = require("node-hue-api").HueApi;
+const auth = require('./auth.json');
+
+var displayResult = function(result) {
+    console.log(JSON.stringify(result, null, 2));
+};
+
+const api = new hue(auth.host, auth.username);
+
+api.getConfig().then(displayResult).done();
